275516d574eccea663e742a87d7c7cd2afcac613
Richard Critz <rcritz@gmail.com>
2024-12-06 19:15:45 -0500

    disable hidden files in nvim neo-tree

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -1,13 +1,22 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+      },
+    },
+  },
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
-  config = function()
+  config = function(_, opts)
+    require("neo-tree").setup(opts)
     vim.keymap.set('n', '<leader>ft', ':Neotree filesystem reveal left<CR>', {})
   end
 }
