7f4ac8175fc54b1cdfdd65768930e4773538bfb6
Richard Critz <rcritz@gmail.com>
2024-12-16 00:41:10 -0500

    wezterm configuration

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config = {
+  automatically_reload_config = true,
+  enable_tab_bar = false,
+  window_close_confirmation = "NeverPrompt",
+  color_scheme = "Catppuccin Latte",
+  font_size = 14.0,
+}
+
+return config
