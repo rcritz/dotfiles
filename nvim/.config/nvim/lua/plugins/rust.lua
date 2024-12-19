fc5957e33b10b6d64e4900a2d5b762efb74dca18
Richard Critz <rcritz@gmail.com>
2024-12-19 16:05:59 -0500

    making progress!! (rust configuration)

--- nvim/.config/nvim/lua/plugins/rust.lua
+++ nvim/.config/nvim/lua/plugins/rust.lua
@@ -2,6 +2,16 @@ return {
   'mrcjkb/rustaceanvim',
   version = '^5', -- Recommended
   lazy = false, -- This plugin is already lazy
+  config = function()
+    vim.g.rustaceanvim = {
+      tools = {
+        hover_actions = {
+          auto_focus = true,
+          replace_builtin_hover = true,
+        },
+      },
+    }
+    end,
   ["rust-analyzer"] = {
     cargo = {
       allFeatures = true,
