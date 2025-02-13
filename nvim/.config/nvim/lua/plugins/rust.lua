2ce4724c18888ceb941e7bdab16b58e3f5b88eb4
Richard Critz <rcritz@gmail.com>
2025-02-13 15:27:03 -0500

    try adding rustowl (doesn't currently seem to work)

--- nvim/.config/nvim/lua/plugins/rust.lua
+++ nvim/.config/nvim/lua/plugins/rust.lua
@@ -1,20 +1,28 @@
 return {
-  'mrcjkb/rustaceanvim',
-  version = '^5', -- Recommended
-  lazy = false, -- This plugin is already lazy
-  config = function()
-    vim.g.rustaceanvim = {
-      tools = {
-        hover_actions = {
-          auto_focus = true,
-          replace_builtin_hover = true,
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^5', -- Recommended
+    lazy = false, -- This plugin is already lazy
+    config = function()
+      vim.g.rustaceanvim = {
+        tools = {
+          hover_actions = {
+            auto_focus = true,
+            replace_builtin_hover = true,
+          },
         },
-      },
-    }
+      }
     end,
-  ["rust-analyzer"] = {
-    cargo = {
-      allFeatures = true,
+    ["rust-analyzer"] = {
+      cargo = {
+        allFeatures = true,
+      },
     },
+  },
+  {
+    "cordx56/rustowl",
+    dependencies = {
+      "neovim/nvim-lspconfig"
+    }
   },
 }
