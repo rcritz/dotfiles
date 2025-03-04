d2aa903dea956b0ac1080067d88a7c004ee37976
Richard Critz <rcritz@gmail.com>
2025-03-04 16:09:44 -0500

    move oil into a floating window

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -3,8 +3,9 @@ return {
   opts = {},
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  lazy = false,
   config = function()
     require("oil").setup()
-    vim.keymap.set("n", "-", "<Cmd>Oil<CR>", { desc = "Open parent directory" })
+    vim.keymap.set("n", "-", "<Cmd>Oil --float<CR>", { desc = "Open parent directory" })
   end
 }
