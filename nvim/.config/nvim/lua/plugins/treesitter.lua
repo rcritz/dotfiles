5133acac1d0d3331e2ce13192584be271fd25be3
Richard Critz <rcritz@gmail.com>
2025-03-05 13:04:18 -0500

    add cool treesitter selection via enter/backspace

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -7,6 +7,15 @@ return {
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<Enter>",
+          node_incremental = "<Enter>",
+          scope_incremental = false,
+          node_decremental = "<Backspace>",
+        },
+      },
     })
   end
 }
