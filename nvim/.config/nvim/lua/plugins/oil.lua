97791e882e3813d21fb372e6ff86c83d43612040
Richard Critz <rcritz@gmail.com>
2025-03-05 13:03:20 -0500

    fix Oil configuration to be a "better" Lazy.nvim citizen

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -4,8 +4,11 @@ return {
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
   lazy = false,
-  config = function()
-    require("oil").setup()
-    vim.keymap.set("n", "-", "<Cmd>Oil --float<CR>", { desc = "Open parent directory" })
-  end
+  keys = {
+    { "-", "<Cmd>Oil --float<CR>", desc = "Open parent directory" },
+  },
+--  config = function()
+--    require("oil").setup()
+--    vim.keymap.set("n", "-", "<Cmd>Oil --float<CR>", { desc = "Open parent directory" })
+--  end
 }
