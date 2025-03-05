9e114eeb31ba603a3ab062d96e5a4692ed0cde81
Richard Critz <rcritz@gmail.com>
2025-03-05 13:48:16 -0500

    clean up tabs

--- nvim/.config/nvim/lua/plugins/debugging.lua
+++ nvim/.config/nvim/lua/plugins/debugging.lua
@@ -1,34 +1,34 @@
 return {
-	{
-		"mfussenegger/nvim-dap",
-		dependencies = {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
       "nvim-lua/plenary.nvim",
-			"rcarriga/nvim-dap-ui",
+      "rcarriga/nvim-dap-ui",
       "nvim-neotest/nvim-nio",
-		},
-		config = function()
-			local dap = require("dap")
-			local dapui = require("dapui")
+    },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
 
-			dapui.setup()
+      dapui.setup()
 
-			dap.listeners.before.attach.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				dapui.close()
-			end
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
 
-			vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, { desc = "toggle breakpoint" })
-			vim.keymap.set("n", "<Leader>dc", dap.continue, { desc = "debug continue" })
+      vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, { desc = "toggle breakpoint" })
+      vim.keymap.set("n", "<Leader>dc", dap.continue, { desc = "debug continue" })
       vim.keymap.set("n", "<Leader>dx", dap.terminate, { desc = "debug terminate" })
       vim.keymap.set("n", "<Leader>do", dap.step_over, { desc = "step over" })
-		end,
-	},
+    end,
+  },
 }
