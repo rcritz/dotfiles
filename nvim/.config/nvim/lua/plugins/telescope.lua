dcca163885a66cfbe2ff4f7ae744b7f84b55130a
Richard Critz <rcritz@gmail.com>
2025-03-12 17:31:12 -0400

    put telescope back, but without any keybindings (at least until fzflua can take over window display)

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -5,12 +5,12 @@ return {
       'nvim-lua/plenary.nvim',
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
-    config = function()
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "find files" })
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "live grep" })
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "find buffers" })
-    end
+--     config = function()
+--       local builtin = require('telescope.builtin')
+--       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "find files" })
+--       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "live grep" })
+--       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "find buffers" })
+--     end
   },
   {
     'nvim-telescope/telescope-ui-select.nvim',
