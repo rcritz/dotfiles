fdca15cc40559a7bf16aa1b3ef3eda15767ff334
Richard Critz <rcritz@gmail.com>
2025-04-21 16:44:54 -0400

    adjust markdown rendering configuration

---
+++ lazyvim/.config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,15 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {
+    code = {
+      sign = true,
+    },
+    heading = {
+      sign = true,
+    },
+    checkbox = {
+      enabled = true,
+    },
+    render_modes = { "n", "c", "t" },
+  },
+}
