7899ffa8beb86bf6da62e4510b552830eba86a6f
Richard Critz <rcritz@gmail.com>
2025-10-17 00:58:00 -0400

    add copilot.lua to neovim config

---
+++ lazyvim/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,20 @@
+return {
+  "zbirenbaum/copilot.lua",
+  opts = {
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      accept = false,
+    },
+    filetypes = {
+      markdown = true,
+      help = false,
+      gitcommit = true,
+      gitrebase = false,
+      hgcommit = false,
+      svn = false,
+      cvs = false,
+      ["."] = true,
+    },
+  },
+}
